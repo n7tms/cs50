524a55cfe93eba44b5587b01d715f672843fbf25
n7tms@yahoo.com <n7tms@yahoo.com>
2023-06-29 13:43:21 -0600

    try_catch

---
+++ number.py
@@ -0,0 +1,17 @@
+def main():
+    x = get_int("What's x? ")
+    print(f"x is {x}")
+
+
+def get_int(prompt):
+    while True:
+        try:
+            x = int(input(prompt))
+        except ValueError:
+            pass
+            # print("x is not an integer")
+        else:
+            return x
+
+
+main()
