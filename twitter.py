ced6df0100cf7281eaac545d5aed395d125921a3
n7tms@yahoo.com <n7tms@yahoo.com>
2023-07-03 16:11:11 -0600

    fix syn err

--- twitter.py
+++ twitter.py
@@ -10,5 +10,5 @@ print(f"Username: {username}")
 
 
 
-if matches:= re.search(r"^https?://(?:www\.)?twitter.com/([a-z0-9_]+)$", url, re.IGNORECASE)
+if matches:= re.search(r"^https?://(?:www\.)?twitter.com/([a-z0-9_]+)$", url, re.IGNORECASE):
     print(f"Username: {matches.group(1)}")
